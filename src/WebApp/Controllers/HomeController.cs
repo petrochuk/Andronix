29e9e5af555f7fb3edc6eace4d79bee6769178d6
Andrew Petrochuk <petrochuk@msn.com>
2024-07-27 11:02:43 -0700

    Refactor HomeController to remove GraphServiceClient

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -1,42 +1,33 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 using WebApp.Models;
-using Microsoft.Graph;
-using Microsoft.Identity.Web;
 
-namespace WebApp.Controllers
+namespace WebApp.Controllers;
+
+[AllowAnonymous]
+public class HomeController : Controller
 {
-    [Authorize]
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly GraphServiceClient _graphServiceClient;
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger, GraphServiceClient graphServiceClient)
-        {
-            _logger = logger;
-            _graphServiceClient = graphServiceClient;;
-        }
+    public Task<IActionResult> Index()
+    {
+        return Task.FromResult<IActionResult>(View());
+    }
 
-        [AuthorizeForScopes(ScopeKeySection = "MicrosoftGraph:Scopes")]
-        public async Task<IActionResult> Index()
-        {
-var user = await _graphServiceClient.Me.Request().GetAsync();
-ViewData["GraphApiResult"] = user.DisplayName;
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [AllowAnonymous]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [AllowAnonymous]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
