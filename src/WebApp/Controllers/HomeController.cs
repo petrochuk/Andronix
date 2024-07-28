1bfbdddb4d38c807b37724acb1b0981082eb836a
Andrew Petrochuk <petrochuk@msn.com>
2024-07-27 18:01:18 -0700

    Refactor controllers and update sidebar navigation

--- src/WebApp/Controllers/HomeController.cs
+++ src/WebApp/Controllers/HomeController.cs
@@ -1,33 +1,21 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using WebApp.Models;
 
 namespace WebApp.Controllers;
 
 [AllowAnonymous]
-public class HomeController : Controller
+public class TopicController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public TopicController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
 
-    public Task<IActionResult> Index()
+    [Route("{**topicName}", Order = 100)]
+    public Task<IActionResult> TopicIndex(string topicName)
     {
-        return Task.FromResult<IActionResult>(View());
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [AllowAnonymous]
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return Task.FromResult<IActionResult>(View("~/Views/Home/Index.cshtml"));
     }
 }
---
+++ src/WebApp/Controllers/TopicController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+[AllowAnonymous]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task<IActionResult> Index()
+    {
+        return Task.FromResult<IActionResult>(View());
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [AllowAnonymous]
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
